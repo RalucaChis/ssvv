4e87d32fa2eaaefe130df96fff0bfa002cc350d4
RalucaChis <48648566+RalucaChis@users.noreply.github.com>
2020-03-15 22:54:21 +0200

    Changed salary

--- ProiectSalariati/src/main/java/salariati/validator/EmployeeValidator.java
+++ ProiectSalariati/src/main/java/salariati/validator/EmployeeValidator.java
@@ -9,11 +9,12 @@ public class EmployeeValidator {
 
 	public boolean isValid(Employee employee) {
 		boolean isLastNameValid  = employee.getLastName().matches("[a-zA-Z]+") && (employee.getLastName().length() > 2);
-		boolean isCNPValid       = employee.getCnp().matches("[a-z0-9]+") && (employee.getCnp().length() == 13);
+		boolean isCNPValid       = employee.getCnp().matches("[0-9]+") && (employee.getCnp().length() == 13);
 		boolean isFunctionValid  = employee.getFunction().equals(DidacticFunction.ASISTENT) ||
 								   employee.getFunction().equals(DidacticFunction.LECTURER) ||
+								   employee.getFunction().equals(DidacticFunction.CONFERENTIAR) ||
 								   employee.getFunction().equals(DidacticFunction.TEACHER);
-		boolean isSalaryValid    = employee.getSalary().matches("[0-9]+") && (employee.getSalary().length() > 1) && (Integer.parseInt(employee.getSalary()) > 0);
+		boolean isSalaryValid    = employee.getSalary()>0;
 		
 		return isLastNameValid && isCNPValid && isFunctionValid && isSalaryValid;
 	}
